5b8222d93ab942086cc4b9e7f99a5a968f5228ec
Mik322 <mchsa@iscte-iul.pt>
2020-04-20 12:52:13 +0100

    Added Topic and Post model and added them to admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Topic, Post
 
 # Register your models here.
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import F
+
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    slug = models.SlugField(max_length=50)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    followers = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="original_poster")
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+
+    def __str__(self):
+        return self.title

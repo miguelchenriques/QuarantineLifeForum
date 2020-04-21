717c37f7e4bed11ff4a0a651c2a16248e4e67f60
Mik322 <mchsa@iscte-iul.pt>
2020-04-21 11:26:01 +0100

    Added popular_topics view

--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Post, Topic, Comment
 from django.core.paginator import Paginator
+from django.db.models import Count
+
+page_size = 10
 
 
 # Create your views here.
 def homepage(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
+    paginator = Paginator(post_list, page_size)
     page_number = request.GET['page']
     page_list = paginator.get_page(page_number)
     context = {
@@ -30,4 +33,16 @@ def post_details(request, post_id):
         'post': post
     }
     # return render(request, 'forum/post_details.html', context)
+    return None
+
+
+def popular_topics(request):
+    topic_list = Topic.objects.annotate(followrs_count=Count('followers')).orderBy('-followers_count')
+    paginator = Paginator(topic_list, page_size)
+    page = request.GET['page']
+    page_list = paginator.get_page(page)
+    context = {
+        'page_list': page_list,
+    }
+    # return render(request, 'forum/popular_topics.html', context)
     return None

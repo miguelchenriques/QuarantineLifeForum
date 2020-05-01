d1906729e576f9a264a272e4b3d254c6cac10101
Mik322 <mchsa@iscte-iul.pt>
2020-05-01 12:55:12 +0100

    Solved Like/s when pushing button

--- forum/static/forum/js/main.js
+++ forum/static/forum/js/main.js
@@ -2,33 +2,27 @@ $(document).ready(function () {
     $(".likeForm").on("submit", function (e) {
         e.preventDefault();
         let this_ = $(this);
-        toggle_like(this_);
+
+        $.ajax({
+            url: this_.attr("action"),
+            method: "Post",
+            data: {
+                post_id: this_.find("input[name=post_id]").val(),
+                csrfmiddlewaretoken: this_.find("input[name=csrfmiddlewaretoken]").val()
+            },
+            success: function (data) {
+                const id = data['post_id']
+                if (data['has_like']) {
+                    this_.find("input:submit").addClass("liked");
+                } else {
+                    this_.find("input:submit").removeClass("liked");
+                }
+                if (data['like_count'] === 1) {
+                    $("p[name="+id+"]").text(data['like_count']+"Like");
+                } else {
+                    $("p[name="+id+"]").text(data['like_count']+"Likes");
+                }
+            }
+        })
     })
 });
-
-
-function toggle_like(this_) {
-    $.ajax({
-        url: this_.attr("action"),
-        method: "Post",
-        data: {
-            post_id: this_.find("input[name=post_id]").val(),
-            csrfmiddlewaretoken: this_.find("input[name=csrfmiddlewaretoken]").val()
-        },
-        success: function (data) {
-            const id = data['post_id']
-            $("p[name="+id+"]").text(data['like_count'])
-            if (data['has_like']) {
-                this_.find("input:submit").addClass("liked");
-            } else {
-                this_.find("input:submit").removeClass("liked");
-            }
-
-            if (data['like_count'] === 1) {
-                this_.find(".like-span").text("Like");
-            } else {
-                this_.find(".like-span").text("Likes");
-            }
-        }
-    })
-}

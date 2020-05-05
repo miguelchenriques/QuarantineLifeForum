50463c77e5bdf62456cbc435c80c6fcf10cc2efb
Mik322 <mchsa@iscte-iul.pt>
2020-05-05 11:45:14 +0100

    Added js that validates username and email

--- forum/static/forum/js/accounts.js
+++ forum/static/forum/js/accounts.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    /* Faz o login utilizando AJAX */
     $("#login_form").on('submit', function (e) {
         e.preventDefault();
         const this_ = $(this);
@@ -23,10 +24,16 @@ $(document).ready(function () {
         });
     })
 
-
+    /*Faz o signUp utilizando AJAX*/
     $("#signup_form").on('submit', function (e) {
         e.preventDefault();
         const this_ = $(this);
+
+
+        if ($("#signup_form #signup_username").hasClass("is_taken")
+            || $("#signup_form signup_email").hasClass("is_taken")) {
+            return;
+        };
 
         $.ajax({
             url: this_.attr('action'),
@@ -45,5 +52,51 @@ $(document).ready(function () {
                 }
             }
         })
+    });
+
+    /* Valida o username */
+    $("#signup_username").change(function () {
+        const this_ = $(this);
+
+        const url_username = $("input[name=url_usernameValidation]").val();
+        $.ajax({
+            url: url_username,
+            method: "get",
+            data: {
+                username: this_.val()
+            },
+            dataType: 'json',
+            success: function (data) {
+                if (data['is_taken']) {
+                    this_.addClass('is_taken')
+                } else {
+                    this_.removeClass('is_taken')
+                }
+            }
+        })
+    });
+
+    /* Valida o email */
+    $("#signup_email").change(function () {
+        const this_ = $(this);
+
+        const url_email = $("input[name=url_emailValidation]").val();
+        $.ajax({
+            url: url_email,
+            method: "get",
+            data: {
+                username: this_.val()
+            },
+            dataType: 'json',
+            success: function (data) {
+                if (data['is_taken']) {
+                    this_.addClass('is_taken')
+                } else {
+                    this_.removeClass('is_taken')
+                }
+            }
+        })
     })
 });
+
+

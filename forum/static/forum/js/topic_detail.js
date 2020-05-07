96f337b593ad1facb017f2ee3890512b0436489a
Afonso Esteves <afonso@MacBook-Pro-de-Afonso.local>
2020-05-07 17:38:34 +0100

    ve la isto

--- forum/static/forum/js/topic_detail.js
+++ forum/static/forum/js/topic_detail.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
 
-})
+    function open_createPost() {
+        $(".createPost").css("display", "flex");
+    };
+
+    $("#createPost-button").click(function () {
+        open_createPost()
+    });
+
+});
